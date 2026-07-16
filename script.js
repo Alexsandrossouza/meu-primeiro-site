9f84d0e214ac555e506addfd4af39451a41964f2
Alexsandrossouza <alexsandrosouza903@gmail.com>
2026-07-16 10:54:01 -0300

    Fecha corretamente a funcao do botao verde

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const botaoVerde = document.querySelector('.btn-verde');
 botaoVerde.addEventListener('click', () => {
     // 3. O que vai acontecer quando clicar: mostrar um alerta na tela!
     alert('Olá, Bem vindo a nossa página, logo com conteúdo novos! 🎉');
+}); // 👈 ESTE FECHAMENTO ESTAVA FALTANDO AQUI!
 
 // 4. Procurando o botão azul no HTML usando a classe dele
 const botaoAzul = document.querySelector('.btn-azul');
@@ -12,5 +13,5 @@ const botaoAzul = document.querySelector('.btn-azul');
 // 5. Dizemos ao botão azul para "ouvir" quando alguém clicar nele
 botaoAzul.addEventListener('click', () => {
     // 6. O que vai acontecer quando clicar: mostrar um alerta na tela!
-    alert('Agora deixe de ser curiosso e para de apertar tudo que vc ver kkkkkkkk');
+    alert('Agora deixe de ser curioso e para de apertar tudo que vc ver kkkkkkkk');
 });
