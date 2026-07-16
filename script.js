b71b4f677261cfb8833a245474e02955c84a9a94
Alexsandrossouza <alexsandrosouza903@gmail.com>
2026-07-16 10:16:17 -0300

    Adiciona alerta divertido no botão azul

--- script.js
+++ script.js
@@ -5,4 +5,12 @@ const botaoVerde = document.querySelector('.btn-verde');
 botaoVerde.addEventListener('click', () => {
     // 3. O que vai acontecer quando clicar: mostrar um alerta na tela!
     alert('Olá, Bem vindo a nossa página, logo com conteúdo novos! 🎉');
+
+// 4. Procurando o botão azul no HTML usando a classe dele
+const botaoAzul = document.querySelector('.btn-azul');
+
+// 5. Dizemos ao botão azul para "ouvir" quando alguém clicar nele
+botaoAzul.addEventListener('click', () => {
+    // 6. O que vai acontecer quando clicar: mostrar um alerta na tela!
+    alert('Agora deixe de ser curiosso e para de apertar tudo que vc ver kkkkkkkk');
 });
