2a756fa2bed455b5872e5918b0b8553a631be3eb
Alexsandro <alexsandro.souza@veczen.com>
2026-07-20 13:53:52 -0300

    Criando grade de jogos automatizada

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-@app.route("/produtos")
-def produtos():
-    return render_template("produtos.html")
-
 @app.route("/jogos")
 def jogos():
-    return render_template("jogos.html")
-
-# ESTE BLOCO DEVE FICAR SEMPRE POR ÚLTIMO:
-if __name__ == "__main__":
-    app.run(debug=True)
+    # Lista de jogos fácil de atualizar!
+    lista_de_jogos = [
+        {
+            "titulo": "Minecraft",
+            "plataforma": "Xbox 360",
+            "tamanho": "1.1 GB",
+            "imagem": "Minecraft-Xbox-360-Edition.jpg",
+            "link": "https://produto.mercadolivre.com.br/MLB-SEU-LINK-AQUI"
+        },
+        {
+            "titulo": "Resident Evil Operation Raccoon City",
+            "plataforma": "Xbox 360",
+            "tamanho": "4.2 GB",
+            "imagem": "Resident-Evil-Operation-Raccoon-City.jpg",
+            "link": "https://produto.mercadolivre.com.br/MLB-SEU-LINK-AQUI"
+        }
+        # Para adicionar um jogo novo, é só copiar e colar um bloco desse aqui!
+    ]
+    return render_template("jogos.html", jogos=lista_de_jogos)
