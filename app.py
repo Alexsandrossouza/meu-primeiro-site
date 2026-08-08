37f7dc6a6336c0d3f735544032bf2c61acd73e30
Alexsandrossouza <alexsandrosouza903@gmail.com>
2026-08-08 16:19:14 -0300

    Adicionando tela de admin

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
 
 # ============================================================
 # 2. ROTA DA PÁGINA DE JOGOS
